446ea874bdd931505ef54056ee166c7337b1926f
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2025-09-16 10:09:16 +0200

    feat: add useLayoutEffect example

--- src/pages/learning/LearnUseEffect.tsx
+++ src/pages/learning/LearnUseEffect.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 const LearnUseEffect = () => {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
+        console.log('Vert');
         console.log("Callback appelé uniquement lors de la création du composant");
         return () => {
             // localStorage.setItem("panier", JSON.stringify(count));
@@ -12,7 +13,15 @@ const LearnUseEffect = () => {
         }
     }, []);
 
+    useLayoutEffect(() => {
+        console.log('Jaune');
+        // useLayout effect est appelé avant le render
+    }, []);
+
+    console.log('Rouge');
+
     useEffect(() => {
+        console.log('Bleu');
         console.log('Abonnement à l\'id ', count);
         return () => {
             console.log('Désabonnement à l\'id ', count);
@@ -24,6 +33,7 @@ const LearnUseEffect = () => {
             <h1>LearnUseEffect</h1>
             <p>Count: {count}</p>
             <button onClick={() => setCount(c => c + 1)}>Increment</button>
+            <button onClick={() => setCount(c => c - 1)}>Decrement</button>
         </section>
     )
 }
