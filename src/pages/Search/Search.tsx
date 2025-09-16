84c5057180f58cc7d1848d3df015c036a9e1a9b3
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2025-09-16 17:06:23 +0200

    refacto: change custom form handling by react-hook-form

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,63 +1,56 @@
-import { useRef, useState } from 'react'
-import { getHeroesByFilters } from '../../services/hero'
-import { HeroAlignment, type Hero } from '../../types/hero'
-import HeroCard from '../../components/HeroCard/HeroCard'
+// import { useState } from 'react'
+// import { getHeroesByFilters } from '../../services/hero'
+// import { HeroAlignment, type Hero } from '../../types/hero'
+// import HeroCard from '../../components/HeroCard/HeroCard'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+
+type Inputs = {
+  name: string
+  hero: string
+}
 
 const Search = () => {
+  const { register, handleSubmit } = useForm<Inputs>()
+  // console.log(unTruc)
   // Utiliser react-hook-form
   // Mettre en place de la validation avec zod
-  const [inputValue, setInputValue] = useState('')
-  const renders = useRef(0)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const alignmentRef = useRef<HTMLSelectElement>(null)
-  const [heroes, setHeroes] = useState<Hero[]>([])
+  // const [inputValue, setInputValue] = useState('')
+  // const renders = useRef(0)
+  // const inputRef = useRef<HTMLInputElement>(null)
+  // const alignmentRef = useRef<HTMLSelectElement>(null)
+  // const [heroes, setHeroes] = useState<Hero[]>([])
 
-  const onSubmitHandler = (event: React.FormEvent) => {
-    event.preventDefault()
-    const alignment = alignmentRef.current?.value as HeroAlignment
-    getHeroesByFilters({ fullname: inputValue, name: inputRef.current?.value, alignment }).then(
-      (heroes) => {
-        setHeroes(heroes)
-      },
-    )
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
   }
 
   return (
     <section>
       <h1>Search</h1>
-      <form onSubmit={onSubmitHandler}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <label htmlFor='alignment'>Alignment</label>
-          <select name='alignment' id='alignment' ref={alignmentRef}>
+          {/* <select name='alignment' id='alignment' ref={alignmentRef}>
             <option value={HeroAlignment.GOOD}>Good</option>
             <option value={HeroAlignment.BAD}>Bad</option>
-          </select>
+          </select> */}
         </fieldset>
         <fieldset>
           <label htmlFor='name'>Name</label>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            value={inputValue}
-            onChange={(event) => {
-              setInputValue(event.target.value)
-              renders.current += 1
-            }}
-          />
+          <input type='text' id='name' {...register('name')} />
         </fieldset>
         <fieldset>
           <label htmlFor='hero'>Hero</label>
-          <input ref={inputRef} type='text' id='hero' name='hero' />
+          <input type='text' id='hero' {...register('hero')} />
         </fieldset>
         <button>Search</button>
       </form>
-      <section className='flex flex-wrap justify-center gap-4'>
+      {/* <section className='flex flex-wrap justify-center gap-4'>
         {heroes.length === 0 && <p>No hero found</p>}
         {heroes.map((hero) => (
           <HeroCard key={hero.id} hero={hero} />
         ))}
-      </section>
+      </section> */}
     </section>
   )
 }
