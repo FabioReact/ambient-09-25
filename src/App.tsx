2b23b2d93c43fd8d61706b0342543e1b44d7300e
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2025-09-18 11:08:46 +0200

    refacto: move routes into specific component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
-import HeroesList from './pages/HeroesList/HeroesList'
-import MainLayout from './layout/MainLayout'
-import HeroDetails from './pages/HeroDetails/HeroDetails'
-import Search from './pages/Search/Search'
-import Register from './pages/Register/Register'
-import Profile from './pages/Profile/Profile'
+import { BrowserRouter } from 'react-router'
 import AuthContextProvider from './providers/AuthContextProvider'
 import CounterContextProvider from './providers/CounterContextProvider'
-import Home from './pages/Home/Home'
 import HeroContextProvider from './providers/HeroContextProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import Login from './pages/Login/Login'
-import PrivateRoute from './hoc/PrivateRoute'
-
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path='/' element={<Home />} />
-        <Route path='/heroes' element={<HeroesList />} />
-        <Route path='/heroes/:id' element={<HeroDetails />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<p>404</p>} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/secret' element={<p>Secret page</p>} />
-        </Route>
-      </Route>
-    </Routes>
-  )
-}
+import AppRoutes from './routes'
 
 const queryClient = new QueryClient()
 
---
+++ src/routes.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router'
+import HeroesList from './pages/HeroesList/HeroesList'
+import MainLayout from './layout/MainLayout'
+import HeroDetails from './pages/HeroDetails/HeroDetails'
+import Search from './pages/Search/Search'
+import Register from './pages/Register/Register'
+import Profile from './pages/Profile/Profile'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import PrivateRoute from './hoc/PrivateRoute'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/heroes' element={<HeroesList />} />
+        <Route path='/heroes/:id' element={<HeroDetails />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<p>404</p>} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/secret' element={<p>Secret page</p>} />
+        </Route>
+      </Route>
+    </Routes>
+  )
+}
+
+export default AppRoutes
